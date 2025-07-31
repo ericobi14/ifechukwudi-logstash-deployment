3712dbd6ed59570ca6bc59a7d88010334da5730c
Ifechukwudi Obiodiaka <ifechukwudiobiodiaka@Mac.fritz.box>
2025-07-31 13:36:49 +0200

    feat: complete VPC networking with NAT, IGW, and route tables

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,6 +1,5 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "${var.project_name}-vpc"
   }
@@ -8,7 +7,6 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-
   tags = {
     Name = "${var.project_name}-igw"
   }
@@ -17,35 +15,29 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-central-1" 
+  availability_zone       = "eu-central-1"
   map_public_ip_on_launch = true
-
   tags = {
     Name = "${var.project_name}-public-subnet"
   }
 }
 
-
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-central-1" 
+  availability_zone       = "eu-central-1"
   map_public_ip_on_launch = true
-
   tags = {
     Name = "${var.project_name}-private-subnet"
   }
 }
 
-
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-
   tags = {
     Name = "${var.project_name}-public-rt"
   }
@@ -56,5 +48,36 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+  depends_on    = [aws_internet_gateway.igw]
 
+  tags = {
+    Name = "${var.project_name}-nat-gw"
+  }
+}
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -13,3 +13,4 @@ variable "public_subnet_cidr" {
 variable "private_subnet_cidr" {
   type = string
 }
+
